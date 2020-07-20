125c91700e94c325441ffe8b138d0981c7ad391e
Daniel <d.p.davison@utwente.nl>
2020-07-20 23:08:30 +0200

    added variation to the chatbox style, that displays buttons with short replies based on the move id

--- js/chatbox.js
+++ js/chatbox.js
@@ -1,9 +1,31 @@
 
 var uiMessages = document.getElementById("messages");
-var uiReplies = document.getElementById("replies");
+
+var uiFullReplies = document.getElementById("full-replies");
+
+var uiShortReplies = document.getElementById("short-replies");
+var uiReplyOptions = document.getElementById("reply-options");
+var uiReplyTextfield = document.getElementById("reply-textfield");
+
+var selectedMove = null;
+
+function toggleStyle(){
+    if(uiFullReplies.style.display !== "none"){
+        uiFullReplies.style.display = "none";
+        uiShortReplies.style.display = "block";
+    } else {
+        uiFullReplies.style.display = "block";
+        uiShortReplies.style.display = "none";
+    }
+}
 
 var displayUserMoves = function(moves){
-    uiReplies.innerHTML = "";
+    generateFullReplies(moves);
+    generateShortReplies(moves);
+};
+
+function generateFullReplies(moves){
+    uiFullReplies.innerHTML = "";
 
     for(let move of moves){
         let button = document.createElement("button");
@@ -11,11 +33,46 @@ var displayUserMoves = function(moves){
         button.classList.add("button", "blue", "alt");
         button.addEventListener ("click", function() {
             selectMove(move);
-            uiReplies.innerHTML = "";
-          });
-          uiReplies.appendChild(button);
+            uiFullReplies.innerHTML = "";
+        });
+        uiFullReplies.appendChild(button);
     }
-};
+}
+
+function generateShortReplies(moves){
+    uiReplyOptions.style.visibility = "hidden";
+    uiReplyOptions.innerHTML = "";
+    uiReplyTextfield.innerHTML = "Your reply";
+    uiReplyTextfield.className = "no-reply-selected";
+    selectedMove = null;
+
+    for(let move of moves){
+        let option = document.createElement("div");
+        option.innerHTML = move.moveID;
+        option.classList.add("option");
+        option.addEventListener ("click", function() {
+            uiReplyOptions.style.visibility = "hidden";
+            uiReplyTextfield.className = "reply-selected";
+            uiReplyTextfield.innerHTML = move.opener;
+            selectedMove = move;
+        });
+        uiReplyOptions.appendChild(option);
+    }
+}
+
+function showShortReplies(){
+    uiReplyOptions.style.visibility = "visible";
+}
+
+function sendSelectedMove(){
+    if(selectedMove){
+        selectMove(selectedMove);
+        selectedMove = null;
+        uiReplyTextfield.className = "no-reply-selected";
+        uiReplyTextfield.innerHTML = "";
+        uiReplyOptions.innerHTML = "";
+    }
+}
 
 var displayMoveStatus = function(status, actorIdentifier, move){
     if(status === "MOVE_REALIZATION"){
